585f90e9cdf5adec71094afcc995633235e69947
Grayson Myers <graymyers10@gmail.com>
2021-01-19 15:18:04 -0700

    Fix issues with webmock

--- spec/features/API/github_service_spec.rb
+++ spec/features/API/github_service_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "GithubService" do
-  before :each do 
-    @path = 'https://api.github.com/repos/cunninghamge/little-esty-shop'
+  before :each do
+    @path = 'https://api.github.com/repos/GrayMyers/little-esty-shop'
     @github = GithubService.new(@path)
   end
   it "is a service class" do
     expect(@github).to be_a GithubService
   end
 
-  describe "instance methods" do 
-    it "get_data" do 
+  describe "instance methods" do
+    it "get_data" do
       expect(@github.get_data).to be_a Hash
     end
 
-    it "get_body" do 
+    it "get_body" do
       expect(@github.get_body(@path)).to be_a Hash
       expect(@github.get_body(@path)).to have_key :full_name
     end
 
-    it "repo_name" do  
+    it "repo_name" do
       expect(@github.repo_name).to be_a Hash
       expect(@github.repo_name).to have_key :full_name
     end
 
-    it "contributors" do 
+    it "contributors" do
       expect(@github.contributors).to be_a Array
       expect(@github.contributors.first).to have_key :login
       expect(@github.contributors.count).to eq(7)
     end
 
-    it "merged_pull_request" do   
+    it "merged_pull_request" do
       expect(@github.merged_pull_request).to be_a Array
       expect(@github.merged_pull_request.first).to have_key :merged_at
     end
