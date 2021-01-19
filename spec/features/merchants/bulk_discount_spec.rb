345284e0ede6173d14e9e65698898ee97c804cd5
Grayson Myers <graymyers10@gmail.com>
2021-01-19 14:03:16 -0700

    Add test for show page links

--- spec/features/merchants/bulk_discount_spec.rb
+++ spec/features/merchants/bulk_discount_spec.rb
@@ -5,6 +5,7 @@ describe "merchant bulk discounts index page" do
     @merchant = create(:merchant)
     @discounts = create_list(:bulk_discount,5,merchant_id: @merchant.id)
     @not_discounts = create_list(:bulk_discount,5)
+    visit merchant_bulk_discounts_path(@merchant.id)
   end
 
   it "lists all merchant discounts" do
@@ -13,6 +14,16 @@ describe "merchant bulk discounts index page" do
         expect(page).to have_content(discount.percent_off)
         expect(page).to have_content(discount.item_quantity)
       end
+    end
+  end
+
+  it "has links to each discount's show page" do
+    @discounts.each do |discount|
+      within("#discount-#{discount.id}") do
+        click_on "See Details"
+        expect(current_path).to eq(merchant_bulk_discount_path(@merchant.id,discount.id))
+      end
+      visit merchant_bulk_discounts_path(@merchant.id)
     end
   end
 
