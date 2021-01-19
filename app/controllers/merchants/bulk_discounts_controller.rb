99d045e500e2d5e74e3b6e118b337401f0a40dfb
Grayson Myers <graymyers10@gmail.com>
2021-01-19 16:37:34 -0700

    Add bulk discount show page

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -1,7 +1,7 @@
 class Merchants::BulkDiscountsController < ApplicationController
   def index
     merchant = Merchant.find(params[:merchant_id])
-    @discounts = merchant.bulk_discounts.active
+    @discounts = merchant.bulk_discounts.enabled 
   end
 
   def show
