fab9227110f0aa4da26d539cf3853cd0d5329087
Grayson Myers <graymyers10@gmail.com>
2021-01-19 16:29:26 -0700

    Add test for enabled method

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -17,4 +17,13 @@ describe BulkDiscount, type: :model do
       expect(@discount.active).to eq(false)
     end
   end
+
+  describe "class methods" do
+    it "enabled" do
+      @discounts = create_list(:bulk_discount, 5)
+      @not_discounts = create_list(:bulk_discount, 5, active: false)
+      expect(BulkDiscount.enabled.to_a).to include(@discounts[0]) #this was returning a nested array for some reason
+      expect(BulkDiscount.enabled.to_a).not_to include(@not_discounts[0])
+    end
+  end
 end
