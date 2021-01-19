6b6e6348f6f7bbff682b4ac5ec3075a4da9ebf0b
Grayson Myers <graymyers10@gmail.com>
2021-01-18 20:09:17 -0700

    Add tests for bulkdiscount relations and validations

---
+++ spec/models/bulk_discount_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe BulkDiscount, type: :model do
+  describe "relations" do
+    it {should have_many :invoice_items}
+    it {should belong_to :merchant}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of :item_quantity}
+    it {should validate_presence_of :percent_off}
+  end
+end
