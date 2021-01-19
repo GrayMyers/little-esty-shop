c4dd165f19329bdbb834aef7c5064e1cb0607eb7
Grayson Myers <graymyers10@gmail.com>
2021-01-18 20:05:56 -0700

    Add migrations for bulk discounts table and refs

---
+++ db/migrate/20210119025558_create_bulk_discounts.rb
@@ -0,0 +1,10 @@
+class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.references :merchant, foreign_key: true
+      t.integer :item_quantity
+      t.integer :percent_off
+      t.timestamps
+    end
+  end
+end
